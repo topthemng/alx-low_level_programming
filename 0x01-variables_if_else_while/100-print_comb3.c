82e8b54895d11e02bb21494f3dab4ea8b33a0272
topthemng <temitopeg86@gmail.com>
2022-12-11 17:14:07 -0800

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = n + 1; m <= 9; m++)
+		{
+			putchar(n + '0');
+			putchar(m + '0');
+			if (n < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

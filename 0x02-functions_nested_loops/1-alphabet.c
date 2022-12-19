e5732ff73a2fc8714faf81aab6fc68a7914a3993
topthemng <temitopeg86@gmail.com>
2022-12-18 16:46:42 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,8 +8,9 @@ void print_alphabet(void)
 {
 	char alpha;
 
-	for (alpha = "a"; alpha <= "z"; alpha++)
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
 		_putchar(alpha);
-
-	_putchar('\n');
+		_putchar('\n');
+	}
 }

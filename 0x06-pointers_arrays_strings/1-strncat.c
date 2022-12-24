5185021d7a1a4a8828103d4dbd09c25ac0373256
topthemng <temitopeg86@gmail.com>
2022-12-23 19:34:09 -0800

    task 1-2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,18 +14,15 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-	i = 0;
-	while (dest[i] != '\0')
+	for (i = 0; dest[i] != '\0'; i++)
 	{
-		i++;
+		continue;
 	}
-	j = 0;
-	while (j < n && src[j] != '\0')
+
+	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
-		dest[i] = src[j]
-		i++;
-		j++;
+		dest[i + j] = src[j];
 	}
-	dest[i] = '\0';
+	dest[i + j] = '\0';
 	return (dest);
 }

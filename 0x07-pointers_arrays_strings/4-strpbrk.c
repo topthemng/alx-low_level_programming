5b1a2db579e60ee44fe6fe76d3e8e77535805e1e
topthemng <temitopeg86@gmail.com>
2023-01-02 14:23:04 -0800

    task-4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -15,7 +15,7 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (j = 0; *(accept + j) != '\n'; j++)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
 				return (s + i);
